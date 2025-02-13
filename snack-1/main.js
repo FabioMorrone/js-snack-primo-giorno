72f5d3f228d9612d18050be0b66d871b370af512
FabioMorrone <morronefabio04@gmail.com>
2025-02-13 11:31:17 +0100

    add

--- snack-1/main.js
+++ snack-1/main.js
@@ -6,3 +6,9 @@ Il software stampa il maggiore.
 
 const aNumber = Number(prompt("inserisci un numero"));
 const theNumber = Number(prompt("inserisci un numero"));
+console.log(aNumber,theNumber);
+
+if (aNumber > theNumber){
+    console.log("il primo numero:");
+    
+}
---
+++ snack-2/main.js
@@ -0,0 +1,15 @@
+/*
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
+
+const parola = (prompt("inserire parola"));
+const parola2 = (prompt("inserire parola"));
+console.log(parola,parola2);
+
+if (parola.length > parola2.length){
+console.log(parola2);
+
+}else if (parola.length < parola2.length){
+    console.log(parola);v
+}
